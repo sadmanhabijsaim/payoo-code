f4b915a39900a6e5c3e903a0496c485a9c05a5e0
sadmanhabijsaim <sadmansiam260@gmail.com>
2026-02-21 03:44:56 +0600

    Implement Backend Javascript

--- script/login.js
+++ script/login.js
@@ -1,1 +1,19 @@
 console.log("Login Functionality Script Loaded");
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    const inputNumber = document.getElementById("input-number");
+    const contactNumber = inputNumber.value;
+    console.log(contactNumber);
+
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+
+    if(contactNumber === "01700000000" && pin === "1234"){
+        alert("Login Successful");
+        window.location.href = "./home.html";
+    }else{
+        alert("Login Failed");
+        return;
+    }
+})
